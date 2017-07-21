d8878d1db3f54cc7bafc4528306263b2ae0a609c
Tom Sanfilippo <sanfilip@gmail.com>
2017-07-21 12:47:30 -0400

    add comments on optional params in sample config

--- src/terraform-provider-paperspace/main.tf
+++ src/terraform-provider-paperspace/main.tf
@@ -1,10 +1,10 @@
 provider "paperspace" {
-  api_key = "1be4f97..."
+  api_key = "1be4f97..." // modify this to use your actual api key
   region = "East Coast (NY2)"
 }
 
 data "paperspace_user" "my-user-1" {
-  email = "me@mycompany.com"
+  email = "me@mycompany.com" // change to the email address of a user on your paperspace team
 }
 
 data "paperspace_template" "my-template-1" {
@@ -25,13 +25,13 @@ EOF
 }
 
 resource "paperspace_machine" "my-machine-1" {
-  region = "East Coast (NY2)" // defaults to provider region if not specified
-  name = "Terraform Test",
+  region = "East Coast (NY2)" // optional, defaults to provider region if not specified
+  name = "Terraform Test"
   machine_type = "C1"
   size = 50
   billing_type = "hourly"
-  assign_public_ip = true
+  assign_public_ip = true // optional, remove if you don't want a public ip assigned
   template_id = "${data.paperspace_template.my-template-1.id}"
-  user_id = "${data.paperspace_user.my-user-1.id}"
-  script_id = "${paperspace_script.my-script-1.id}"
+  user_id = "${data.paperspace_user.my-user-1.id}"  // optional, remove to default
+  script_id = "${paperspace_script.my-script-1.id}" // optional, remove for no script
 }
