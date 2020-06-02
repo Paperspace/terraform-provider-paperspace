9041f39fed93eb8a7f1faa2f7cc9841d5fc8c5b9
Jared Scheib <jared.scheib@gmail.com>
2020-06-02 17:56:32 -0400

    go fmt

--- src/terraform-provider-paperspace/client.go
+++ src/terraform-provider-paperspace/client.go
@@ -1,52 +1,52 @@
 package main
 
 import (
-  "github.com/hashicorp/terraform/helper/schema"
-  "gopkg.in/resty.v0"
-  "log"
-  "reflect"
+	"github.com/hashicorp/terraform/helper/schema"
+	"gopkg.in/resty.v0"
+	"log"
+	"reflect"
 )
 
 type MapIf map[string]interface{}
 
 func (m *MapIf) Append(d *schema.ResourceData, k string) {
-  v := d.Get(k)
-  (*m)[k] = v
+	v := d.Get(k)
+	(*m)[k] = v
 }
 
-func (m *MapIf) AppendAs(d *schema.ResourceData, k,nk string) {
-  v := d.Get(k)
-  (*m)[nk] = v
+func (m *MapIf) AppendAs(d *schema.ResourceData, k, nk string) {
+	v := d.Get(k)
+	(*m)[nk] = v
 }
 
-func (m *MapIf) AppendV(d *schema.ResourceData, k,v string) {
-  (*m)[k] = v
+func (m *MapIf) AppendV(d *schema.ResourceData, k, v string) {
+	(*m)[k] = v
 }
 
 func (m *MapIf) AppendIfSet(d *schema.ResourceData, k string) {
-  v := d.Get(k)
-  if reflect.ValueOf(v).Interface() != reflect.Zero(reflect.TypeOf(v)).Interface() {
-    (*m)[k] = v
-  }
+	v := d.Get(k)
+	if reflect.ValueOf(v).Interface() != reflect.Zero(reflect.TypeOf(v)).Interface() {
+		(*m)[k] = v
+	}
 }
 
-func (m *MapIf) AppendAsIfSet(d *schema.ResourceData, k,nk string) {
-  v := d.Get(k)
-  if reflect.ValueOf(v).Interface() != reflect.Zero(reflect.TypeOf(v)).Interface() {
-    (*m)[nk] = v
-  }
+func (m *MapIf) AppendAsIfSet(d *schema.ResourceData, k, nk string) {
+	v := d.Get(k)
+	if reflect.ValueOf(v).Interface() != reflect.Zero(reflect.TypeOf(v)).Interface() {
+		(*m)[nk] = v
+	}
 }
 
 func SetResDataFrom(d *schema.ResourceData, m map[string]interface{}, dn, n string) {
-  v, ok := m[n]
-  //log.Printf("%v %v\n", n, v)
-  if ok {
-    d.Set(dn, v)
-  }
+	v, ok := m[n]
+	//log.Printf("%v %v\n", n, v)
+	if ok {
+		d.Set(dn, v)
+	}
 }
 
 func SetResData(d *schema.ResourceData, m map[string]interface{}, n string) {
-  SetResDataFrom(d, m, n, n)
+	SetResDataFrom(d, m, n, n)
 }
 
 type Config struct {
@@ -56,37 +56,37 @@ type Config struct {
 }
 
 type PaperspaceClient struct {
-  ApiKey  string
-	ApiHost string
-	Region  string
+	ApiKey      string
+	ApiHost     string
+	Region      string
 	RestyClient *resty.Client
 }
 
 func (c *Config) Client() (PaperspaceClient, error) {
 
-  restyClient := resty.New();
+	restyClient := resty.New()
 
-  restyClient.
-  SetHostURL(c.ApiHost).
-  SetHeader("x-api-key", c.ApiKey).
-  SetHeader("Accept", "application/json")
+	restyClient.
+		SetHostURL(c.ApiHost).
+		SetHeader("x-api-key", c.ApiKey).
+		SetHeader("Accept", "application/json")
 
 	client := PaperspaceClient{
-		ApiKey:  c.ApiKey,
-		ApiHost: c.ApiHost,
-		Region:  c.Region,
-    RestyClient: restyClient,
+		ApiKey:      c.ApiKey,
+		ApiHost:     c.ApiHost,
+		Region:      c.Region,
+		RestyClient: restyClient,
 	}
 
 	return client, nil
 }
 
 func LogResponse(reqDesc string, resp *resty.Response, err error) {
-  log.Printf("Request: %v", reqDesc)
-  log.Printf("Error: %v", err)
-  log.Printf("Response Status Code: %v", resp.StatusCode())
-  log.Printf("Response Status: %v", resp.Status())
-  log.Printf("Response Time: %v", resp.Time())
-  log.Printf("Response Recevied At: %v", resp.ReceivedAt())
-  log.Printf("Response Body: %v", resp) // or resp.String() or string(resp.Body())
+	log.Printf("Request: %v", reqDesc)
+	log.Printf("Error: %v", err)
+	log.Printf("Response Status Code: %v", resp.StatusCode())
+	log.Printf("Response Status: %v", resp.Status())
+	log.Printf("Response Time: %v", resp.Time())
+	log.Printf("Response Recevied At: %v", resp.ReceivedAt())
+	log.Printf("Response Body: %v", resp) // or resp.String() or string(resp.Body())
 }
--- src/terraform-provider-paperspace/provider.go
+++ src/terraform-provider-paperspace/provider.go
@@ -1,46 +1,45 @@
 package main
 
 import (
-  "github.com/hashicorp/terraform/helper/schema"
-  "log"
-  "os"
+	"github.com/hashicorp/terraform/helper/schema"
+	"log"
+	"os"
 )
 
 func Provider() *schema.Provider {
-  return &schema.Provider{
-    Schema: map[string]*schema.Schema{
-      "api_key": &schema.Schema{
-        Type:        schema.TypeString,
-        Optional:    true,
-        DefaultFunc: envDefaultFunc("PAPERSPACE_API_KEY"),
-      },
-      "api_host": &schema.Schema{
-        Type:        schema.TypeString,
-        Optional:    true,
-        DefaultFunc: envDefaultFuncAllowMissingDefault("PAPERSPACE_API_HOST", "https://api.paperspace.io"),
-      },
-      "region": &schema.Schema{
-        Type:        schema.TypeString,
-        Optional:    true,
-        DefaultFunc: envDefaultFuncAllowMissing("PAPERSPACE_REGION"),
-      },
-    },
+	return &schema.Provider{
+		Schema: map[string]*schema.Schema{
+			"api_key": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: envDefaultFunc("PAPERSPACE_API_KEY"),
+			},
+			"api_host": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: envDefaultFuncAllowMissingDefault("PAPERSPACE_API_HOST", "https://api.paperspace.io"),
+			},
+			"region": &schema.Schema{
+				Type:        schema.TypeString,
+				Optional:    true,
+				DefaultFunc: envDefaultFuncAllowMissing("PAPERSPACE_REGION"),
+			},
+		},
 
-    ResourcesMap: map[string]*schema.Resource{
-      "paperspace_machine":  resourceMachine(),
-      "paperspace_script":   resourceScript(),
-    },
+		ResourcesMap: map[string]*schema.Resource{
+			"paperspace_machine": resourceMachine(),
+			"paperspace_script":  resourceScript(),
+		},
 
-    DataSourcesMap: map[string]*schema.Resource{
-      "paperspace_network":  dataSourceNetwork(),
-      "paperspace_template": dataSourceTemplate(),
-      "paperspace_user":     dataSourceUser(),
-    },
+		DataSourcesMap: map[string]*schema.Resource{
+			"paperspace_network":  dataSourceNetwork(),
+			"paperspace_template": dataSourceTemplate(),
+			"paperspace_user":     dataSourceUser(),
+		},
 
-    ConfigureFunc: providerConfigure,
-  }
+		ConfigureFunc: providerConfigure,
+	}
 }
-
 
 func envDefaultFunc(k string) schema.SchemaDefaultFunc {
 	return func() (interface{}, error) {
@@ -61,26 +60,26 @@ func envDefaultFuncAllowMissing(k string) schema.SchemaDefaultFunc {
 
 func envDefaultFuncAllowMissingDefault(k string, d string) schema.SchemaDefaultFunc {
 	return func() (interface{}, error) {
-    if v := os.Getenv(k); v != "" {
+		if v := os.Getenv(k); v != "" {
 			return v, nil
 		}
 
-    return d, nil
+		return d, nil
 	}
 }
 
 func providerConfigure(d *schema.ResourceData) (interface{}, error) {
 	config := Config{
-		ApiKey: d.Get("api_key").(string),
+		ApiKey:  d.Get("api_key").(string),
 		ApiHost: d.Get("api_host").(string),
-		Region: d.Get("region").(string),
+		Region:  d.Get("region").(string),
 	}
 
-  log.Printf("[INFO] paperspace provider api_key %v", config.ApiKey)
-  log.Printf("[INFO] paperspace provider api_host %v", config.ApiHost)
-  if config.Region != "" {
-    log.Printf("[INFO] paperspace provider region %v", config.Region)
-  }
+	log.Printf("[INFO] paperspace provider api_key %v", config.ApiKey)
+	log.Printf("[INFO] paperspace provider api_host %v", config.ApiHost)
+	if config.Region != "" {
+		log.Printf("[INFO] paperspace provider region %v", config.Region)
+	}
 
 	return config.Client()
 }
