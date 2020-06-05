0441da124bc218d3528e03dcade4d57c0e2b22e3
Jared Scheib <jared.scheib@gmail.com>
2020-06-05 14:24:26 -0400

    fix: add team id & comment unnecessary params

--- src/terraform-provider-paperspace/main.tf
+++ src/terraform-provider-paperspace/main.tf
@@ -1,37 +1,39 @@
 provider "paperspace" {
-  api_key = "1be4f97..." // modify this to use your actual api key
   region = "East Coast (NY2)"
-}
-
-data "paperspace_user" "my-user-1" {
-  email = "me@mycompany.com" // change to the email address of a user on your paperspace team
+  api_key = "12345678..."
 }
 
 data "paperspace_template" "my-template-1" {
-  label = "Ubuntu 16.04 Server"
+  label = "Ubuntu 18.04 Server"
 }
 
-resource "paperspace_script" "my-script-1" {
-  name = "My Script"
-  description = "a short description"
-  script_text = <<EOF
-#!/bin/bash
-echo "Hello, World" > index.html
-ufw allow 8080
-nohup busybox httpd -f -p 8080 &
-EOF
-  is_enabled = true
-  run_once = false
+data "paperspace_user" "my-user-1" {
+  email = "user@email.com"
+  team_id = "te1234567"
 }
+
+# resource "paperspace_script" "my-script-1" {
+#   name = "My Script"
+#   description = "a short description"
+#   script_text = <<EOF
+# #!/bin/bash
+# echo "Hello, World" > index.html
+# ufw allow 8080
+# nohup busybox httpd -f -p 8080 &
+# EOF
+#   is_enabled = true
+#   run_once = false
+# }
 
 resource "paperspace_machine" "my-machine-1" {
   region = "East Coast (NY2)" // optional, defaults to provider region if not specified
-  name = "Terraform Test"
+  name = "dev-gradient-terraform-test-1"
   machine_type = "C1"
   size = 50
   billing_type = "hourly"
   assign_public_ip = true // optional, remove if you don't want a public ip assigned
   template_id = data.paperspace_template.my-template-1.id
   user_id = data.paperspace_user.my-user-1.id  // optional, remove to default
-  script_id = paperspace_script.my-script-1.id // optional, remove for no script
+  team_id = data.paperspace_user.my-user-1.team_id
+  # script_id = paperspace_script.my-script-1.id // optional, remove for no script
 }
