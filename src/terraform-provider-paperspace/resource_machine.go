ad679ad2759bf9a6ff52778792dec401381fc167
Jared Scheib <jared.scheib@gmail.com>
2020-07-02 17:52:45 -0400

    refactor(machines): remove obsolete cluster_idfrom machines

--- src/terraform-provider-paperspace/resource_machine.go
+++ src/terraform-provider-paperspace/resource_machine.go
@@ -34,7 +34,6 @@ func resourceMachineCreate(d *schema.ResourceData, m interface{}) error {
 	body.AppendAsIfSet(d, "team_id", "teamId")
 	body.AppendAsIfSet(d, "script_id", "scriptId")
 	body.AppendAsIfSet(d, "network_id", "networkId")
-	body.AppendAsIfSet(d, "cluster_id", "clusterId")
 
 	// fields not tested when this project was picked back up for https://github.com/Paperspace/terraform-provider-paperspace/pull/3
 	body.AppendIfSet(d, "email")
@@ -88,7 +87,6 @@ func resourceMachineCreate(d *schema.ResourceData, m interface{}) error {
 		SetResDataFrom(d, body, "team_id", "teamId")
 		SetResDataFrom(d, body, "script_id", "scriptId")
 		SetResDataFrom(d, body, "dt_last_run", "dtLastRun")
-		SetResDataFrom(d, body, "cluster_id", "clusterId")
 
 		d.SetId(id)
 
@@ -130,7 +128,6 @@ func resourceMachineRead(d *schema.ResourceData, m interface{}) error {
 	SetResDataFrom(d, mp, "team_id", "teamId")
 	SetResDataFrom(d, mp, "script_id", "scriptId")
 	SetResDataFrom(d, mp, "dt_last_run", "dtLastRun")
-	SetResDataFrom(d, mp, "cluster_id", "clusterId")
 
 	return nil
 }
@@ -311,10 +308,6 @@ func resourceMachine() *schema.Resource {
 			},
 			"public_ip_address": &schema.Schema{
 				Type:     schema.TypeString,
-				Computed: true,
-			},
-			"cluster_id": &schema.Schema{
-				Type:     schema.TypeInt,
 				Computed: true,
 			},
 		},
