7e1efbe2cfa866ab7a02418af55ab11aa27429c3
Philip Reichenberger <philip@paperspace.com>
2020-07-16 11:45:48 -0400

    checkpoint

--- src/terraform-provider-paperspace/resource_job_storage.go
+++ src/terraform-provider-paperspace/resource_job_storage.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
@@ -61,37 +60,6 @@ func resourceJobStorageUpdate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceJobStorageDelete(d *schema.ResourceData, m interface{}) error {
-	return nil
-}
-
-func dataSourceJobStorageRead(d *schema.ResourceData, m interface{}) error {
-	paperspaceClient := m.(PaperspaceClient)
-	region := paperspaceClient.Region
-
-	teamID, ok := d.Get("team_id").(int)
-	if !ok {
-		return fmt.Errorf("team_id is not a int")
-	}
-	regionData, ok := d.Get("region").(string)
-	if !ok {
-		return fmt.Errorf("region is not a string")
-	}
-
-	if regionData != "" {
-		region = regionData
-	}
-
-	jobStorage, err := paperspaceClient.GetJobStorageByRegion(teamID, region)
-	if err != nil {
-		return err
-	}
-	if jobStorage.Handle == "" {
-		return errors.New("Could not find job storage")
-	}
-
-	d.SetId(jobStorage.Handle)
-	updateJobStorageSchema(d, jobStorage)
-
 	return nil
 }
 
