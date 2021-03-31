a81d3b97e958c1defcf56a7eed6880c74603cf7c
Philip Reichenberger <philip@paperspace.com>
2021-03-30 21:40:04 -0400

    Update goreleaser

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/Paperspace/terraform-paperspace-provider/pkg/provider"
 	"github.com/hashicorp/terraform-plugin-sdk/plugin"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -8,7 +9,7 @@ import (
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() terraform.ResourceProvider {
-			return Provider()
+			return provider.Provider()
 		},
 	})
 }
